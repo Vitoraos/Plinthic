aa67b5c42cf76be6489a31ad80747e772128c751
Vitora OS <emmykarl07@gmail.com>
2026-07-01 04:31:24 +0100

    Add error handling types and ParseError struct

---
+++ internal/parser/errors.go
@@ -0,0 +1,35 @@
+package parser
+
+import "fmt"
+
+type ErrorCode string
+
+const (
+	ErrParseFailed                ErrorCode = "PARSE_FAILED"
+	ErrInvalidEncoding            ErrorCode = "PARSE_FAILED_INVALID_ENCODING"
+	ErrSkippedSymlinkCycle        ErrorCode = "SKIPPED_SYMLINK_CYCLE"
+	ErrSkippedPermission          ErrorCode = "SKIPPED_PERMISSION_DENIED"
+	ErrRepoTooLarge               ErrorCode = "REPO_TOO_LARGE"
+	ErrPanicRecovered             ErrorCode = "PANIC_RECOVERED"
+	ErrCannotStaticallyDetermine  ErrorCode = "CANNOT_STATICALLY_DETERMINE"
+	ErrUnknownCardinality         ErrorCode = "UNKNOWN_CARDINALITY"
+)
+
+// ParseError attaches to one file (never aborts the whole walk).
+type ParseError struct {
+	Code    ErrorCode
+	File    string
+	Line    int
+	Message string
+}
+
+func (e *ParseError) Error() string {
+	if e.Line > 0 {
+		return fmt.Sprintf("%s: %s:%d: %s", e.Code, e.File, e.Line, e.Message)
+	}
+	return fmt.Sprintf("%s: %s: %s", e.Code, e.File, e.Message)
+}
+
+func NewParseError(code ErrorCode, file string, line int, message string) *ParseError {
+	return &ParseError{Code: code, File: file, Line: line, Message: message}
+}
